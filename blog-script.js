7c3165052d2b3715659dd1417e2fa5d0b023b3e0
mushtaq <75521992+goatmanking@users.noreply.github.com>
2024-06-14 15:22:02 +0500

    Website with about me
    first commit

---
+++ blog-script.js
@@ -0,0 +1,8 @@
+const submitButton = document.getElementById("blog-submit");
+const blogText = document.getElementById("blog-textarea");
+const labelwa = document.getElementById("labelwa");
+
+function submitBlogFun() {
+	document.createElement;
+	labelwa.innerText = blogText.value;
+}
