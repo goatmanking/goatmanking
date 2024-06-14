70df3b9dc8829f3837e992b3ca184a9ab71eead1
mushtaq <75521992+goatmanking@users.noreply.github.com>
2024-06-14 23:13:06 +0500

    Re-imagining blog

--- blog-script.js
+++
@@ -1,8 +0,0 @@
-const submitButton = document.getElementById("blog-submit");
-const blogText = document.getElementById("blog-textarea");
-const labelwa = document.getElementById("labelwa");
-
-function submitBlogFun() {
-	document.createElement;
-	labelwa.innerText = blogText.value;
-}
